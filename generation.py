Fixed perplexity calculation

generation.py
@@ -2,7 +2,9 @@
 
 # System Modules
 import torch
-from typing import List, Union
+import torch.nn.functional as F
+
+from typing import List, Union, Tuple, Dict
 
 from transformers import (
     AutoModelForCausalLM,
@@ -25,6 +27,7 @@ class GenerationBase:
 
         self.model = model
         self.model = self.model.to(device)
+        self.model.eval()
         self.tokenizer = tokenizer
         self.device = device
         self.beam_width = beam_width
@@ -45,16 +48,30 @@ class GODEL(GenerationBase):
 
         self.instruction = "Instruction: given a dialog context, you need to response empathically."
 
-    def generate(self, batch: List[List[str]]) -> List[List[int]]:
-        batch = [f"{self.instruction} [CONTEXT] {' EOS '.join(history)}" for history in batch]
-        input_ids = self.tokenizer(batch, return_tensors="pt", padding=True).input_ids
-        input_ids = input_ids.to(self.device)
-        outputs = self.model.generate(
-            input_ids,
+    def generate(self, batch: Dict[str, Tuple]) -> Tuple[List[List[int]], float]:
+        _input = [f"{self.instruction} [CONTEXT] {' EOS '.join(dialogue)}"
+                  for dialogue in batch["contexts"]]
+        _input = self.tokenizer(_input, return_tensors="pt", padding=True).to(self.device)
+        decoder_input = self.tokenizer(batch["targets"], return_tensors="pt", padding=True).to(self.device)
+        output = self.model(
+            input_ids=_input.input_ids,
+            attention_mask=_input.attention_mask,
+            decoder_input_ids=decoder_input.input_ids,
+            decoder_attention_mask=decoder_input.attention_mask
+        )
+        cross_entropy = F.cross_entropy(
+            output.logits[:, :-1, :].permute(0, 2, 1),
+            decoder_input.input_ids[:, 1:],
+            ignore_index=self.tokenizer.pad_token_id
+        )
+
+        predictions = self.model.generate(
+            _input.input_ids,
             pad_token_id=self.tokenizer.pad_token_id,
             max_length=self.max_len,
             num_beams=self.beam_width
         )
-        return outputs.tolist()
+
+        return predictions.tolist(), cross_entropy
 
 # ----------------------------------------------------------------------------

metric_utils.py
@@ -20,9 +20,8 @@ def compute_test_metrics(
     predictions: List[str],
     encoded_targets: List[List[int]],
     encoded_predictions: List[List[int]],
-    pred_n_grams: int = 4,
-    log_prob: Optional[float] = None,
     word_embeddings: Optional[nn.Module] = None,
+    pred_n_grams: int = 4,
 ) -> Dict[str, float]:
 
     n_unigrams, n_bigrams = 0, 0
@@ -82,9 +81,6 @@ def compute_test_metrics(
         "dist-2": len(unique_bigrams) / n_bigrams
     }
 
-    if log_prob is not None:
-        num_tokens = sum([len(enc) - 1 for enc in encoded_predictions])
-        test_metrics["ppl"] = math.exp(-log_prob/num_tokens)
     if word_embeddings is not None:
         test_metrics["avg_bow"] = sum(avg_bow) / len(avg_bow)
         test_metrics["extrema_bow"] = sum(extrema_bow) / len(extrema_bow)

model_supervisor.py
@@ -2,6 +2,7 @@
 
 # System Modules
 import json
+import math
 from typing import Dict, List, Tuple, Union
 
 import torch
@@ -108,10 +109,9 @@ class ModelSupervisor(pl.LightningModule):
         return avg_val_loss
 
     def on_test_start(self) -> None:
-        self.log_prob = 0
         (self.contexts, self.targets, self.emotions,
          self.predictions, self.enc_targets, self.enc_predictions,
-         self.emo_predictions, self.concepts) = ([] for _ in range(8))
+         self.emo_predictions, self.concepts, self.cross_entropy) = ([] for _ in range(9))
 
     def test_step(self, batch: Dict, _) -> Tuple[List]:
         targets = [self.tokenizer.decode_to_text(enc) for enc in batch["target"].tolist()]
@@ -119,10 +119,16 @@ class ModelSupervisor(pl.LightningModule):
         self.enc_targets.extend([self.tokenizer.encode_text(target, "target")[0]
                                 for target in targets])
 
-        enc_prediction, log_prob = self.beam_search(batch)
+        logits, target_seq = self.forward(batch)
+        self.cross_entropy.append(F.cross_entropy(
+            logits[:, :-1, :].permute(0, 2, 1),
+            target_seq,
+            ignore_index=self.tokenizer.PAD_IDX
+        ))
+
+        enc_prediction = self.beam_search(batch)
         self.enc_predictions.extend([enc_prediction])
         self.predictions.extend([self.tokenizer.decode_to_text(enc_prediction)])
-        self.log_prob += log_prob
 
         self.contexts.extend([self.tokenizer.decode_to_text(context)
                              for context in batch["context"].tolist()])
@@ -162,10 +168,11 @@ class ModelSupervisor(pl.LightningModule):
             self.predictions,
             self.enc_targets,
             self.enc_predictions,
-            self.pred_n_grams,
-            self.log_prob,
             self.model.word_embeddings,
+            self.pred_n_grams,
         )
+
+        test_metrics["ppl"] = math.exp(sum(self.cross_entropy) / len(self.cross_entropy))
 
         if len(self.emo_predictions) != 0:
             test_metrics["emo_accuracy"] = accurate_emo_labels / N
@@ -246,10 +253,7 @@ class ModelSupervisor(pl.LightningModule):
             eos_detected = [self.tokenizer.EOS_IDX in seq for seq in beam]
 
         # Return most probable sequence and its log probability for each beam in batch
-        prediction = beam[0].tolist()
-        log_prob = float(beam_prob[0])
-
-        return prediction, log_prob
+        return beam[0].tolist()
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
         optimizer = torch.optim.AdamW(

test_pretrained.py
@@ -2,10 +2,11 @@
 
 # System Modules
 import os
+import math
 import json
 import argparse
 from tqdm import tqdm
-from typing import Iterable
+from typing import List, Dict, Tuple
 
 import torch
 
@@ -48,33 +49,40 @@ def main():
         contexts = json.load(f)
     with open(f"{dataset_dir}/test/targets.json") as f:
         targets = json.load(f)
-    with open(f"{dataset_dir}/test/emotions.json") as f:
-        emotions = json.load(f)
 
     class TestDataset(torch.utils.data.Dataset):
-        def __init__(self, contexts: Iterable) -> None:
+        def __init__(self, contexts: list, targets: list) -> None:
             self.contexts = contexts
+            self.targets = targets
 
         def __len__(self) -> int:
             return len(self.contexts)
 
         def __getitem__(self, idx: int) -> list:
-            return self.contexts[idx]
+            return self.contexts[idx], self.targets[idx]
+
+    def collate_fn(batch: List[Tuple]) -> Dict[str, Tuple]:
+        contexts, targets = zip(*batch)
+        return {
+            "contexts": contexts,
+            "targets": targets
+        }
 
     test_dataloader = torch.utils.data.DataLoader(
-        TestDataset(contexts),
+        TestDataset(contexts, targets),
         batch_size=cli_args.batch_size,
         num_workers=max(1, os.cpu_count() // 4),
-        collate_fn=lambda x: x
+        collate_fn=collate_fn
     )
 
     print("Generating predictions from pretrained model...")
-    predictions, enc_predictions = [], []
-    for batch in tqdm(test_dataloader):
-        outputs = model_generation.generate(batch)
-        enc_predictions.extend(outputs)
-        predictions.extend([tokenizer.decode(enc, skip_special_tokens=True)
-                            for enc in outputs])
+    predictions, enc_predictions, cross_entropy = [], [], []
+    with torch.no_grad():
+        for batch in tqdm(test_dataloader):
+            preds, nll = model_generation.generate(batch)
+            cross_entropy.append(nll)
+            enc_predictions.extend(preds)
+            predictions.extend([tokenizer.decode(enc, skip_special_tokens=True) for enc in preds])
     print("Done.")
 
     os.makedirs(cli_args.output_dir, exist_ok=True)
@@ -89,9 +97,6 @@ def main():
             "context": contexts[i],
             "target": targets[i],
             "prediction": predictions[i],
-            "emotion": emotions[i],
-            "pred_emotion": None,
-            "concepts": None
         }
         test_data.append(entry)
 
@@ -101,17 +106,17 @@ def main():
 
     print(f"Test data saved at '{fname}'")
 
-    print("Computing test metrics")
+    print("Computing test metrics...")
     enc_targets = [tokenizer(seq).input_ids for seq in targets]
     test_metrics = compute_test_metrics(
         targets,
         predictions,
         enc_targets,
         enc_predictions,
+        model.get_input_embeddings(),
         cli_args.pred_n_grams,
-        None,
-        model.get_input_embeddings()
     )
+    test_metrics["ppl"] = math.exp(sum(cross_entropy) / len(cross_entropy))
     print("Test metrics computed.")
 
     fname = f"{dir}/test_metrics.json"
Added generation config class. Fixed perplexity calculation in pretrained models

generation.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn.functional as F
 
 # User-Defined Modules
-from data_classes import EncoderDecoderModelBatch, DecoderModelBatch
+from data_classes import EncoderDecoderModelBatch, DecoderModelBatch, GenerationConfig
 
 # ------------------------- IMPLEMENTATION -----------------------------------
 
@@ -42,14 +42,16 @@ def generate(
     stop_token: int,
     pad_token: int,
     vocab_size: int,
-    max_new_tokens: int = 100,
-    beam_width: int = 1,
-    sample: bool = False,
-    temperature: float = 1.0,
-    top_p: float = 1.0,
-    top_k: int = 50,
+    generation_config: GenerationConfig,
     start_token: Optional[int] = None,
 ) -> torch.LongTensor:
+
+    max_new_tokens = generation_config.max_new_tokens
+    beam_width = generation_config.beam_width
+    sample = generation_config.sample
+    temperature = generation_config.temperature
+    top_p = generation_config.top_p
+    top_k = generation_config.top_k
 
     if model_has_encoder:
         N = batch.contexts.size(dim=0)
@@ -124,7 +126,15 @@ def generate(
         if stop:
             break
 
-    # Return most probable sequence and its log probability for each beam in batch
-    return beams[:, 0]
+    # Return most probable sequence for each beam in batch
+    sequences = beams[:, 0].tolist()
+    for i in range(len(sequences)):
+        try:
+            ind = sequences[i].index(stop_token) + 1
+        except ValueError:
+            ind = len(sequences[i])
+        sequences[i] = sequences[i][:ind]
+    
+    return sequences
 
 # -----------------------------------------------------------------------------

model_supervisor.py
@@ -14,7 +14,7 @@ from transformers.optimization import get_linear_schedule_with_warmup
 
 # User-defined Modules
 from base_classes import DialogueModelBase, TokenizerBase
-from data_classes import EncoderDecoderModelBatch, DecoderModelBatch
+from data_classes import EncoderDecoderModelBatch, DecoderModelBatch, GenerationConfig
 from metric_utils import compute_test_metrics
 from generation import generate
 
@@ -30,18 +30,17 @@ class ModelSupervisor(pl.LightningModule):
         initial_lr: Optional[float] = None,
         metric_n_grams: Optional[int] = None,
         test_output_dir: Optional[str] = None,
-        generation_kwargs: Optional[Dict] = None,
+        generation_config: Optional[GenerationConfig] = None,
     ) -> None:
 
         super().__init__()
-        self.save_hyperparameters(ignore=["tokenizer", "model"])
 
         self.model = model
         self.tokenizer = tokenizer
         self.initial_lr = initial_lr
         self.batch_size = batch_size
         self.test_output_dir = test_output_dir
-        self.generation_kwargs = generation_kwargs
+        self.generation_config = generation_config
         self.metric_n_grams = metric_n_grams
 
     def forward(
@@ -142,7 +141,7 @@ class ModelSupervisor(pl.LightningModule):
         self.enc_targets.extend([self.tokenizer.encode_text(target)[0]
                                  for target in targets])
 
-        enc_predictions = self.generate(batch).tolist()
+        enc_predictions = self.generate(batch)
         self.enc_predictions.extend(enc_predictions)
         self.predictions.extend([self.tokenizer.decode_to_text(enc) 
                                  for enc in enc_predictions])
@@ -236,8 +235,9 @@ class ModelSupervisor(pl.LightningModule):
             pad_token=self.tokenizer.PAD_IDX,
             vocab_size=self.tokenizer.vocab_size,
             model_has_encoder=self.model.has_encoder,
-            **self.generation_kwargs
+            generation_config=self.generation_config
         )
+
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
         optimizer = torch.optim.AdamW(

pretrained_gen.py
@@ -12,6 +12,9 @@ from transformers import (
     AutoTokenizer
 )
 
+# User-Defined Modules
+from data_classes import GenerationConfig
+
 # ------------------------- IMPLEMENTATION -----------------------------------
 
 
@@ -21,8 +24,7 @@ class PretrainedGenerationBase:
         model: Union[AutoModelForSeq2SeqLM, AutoModelForCausalLM],
         tokenizer: AutoTokenizer,
         device: torch.device,
-        beam_width: int,
-        max_len: int
+        generation_config: GenerationConfig
     ) -> None:
 
         self.model = model
@@ -30,25 +32,23 @@ class PretrainedGenerationBase:
         self.model.eval()
         self.tokenizer = tokenizer
         self.device = device
-        self.beam_width = beam_width
-        self.max_len = max_len
+        self.generation_config = generation_config
 
 
 class GODEL(PretrainedGenerationBase):
     def __init__(
         self,
         device: torch.device,
-        beam_width: int,
-        max_len: int
+        generation_config: GenerationConfig
     ) -> None:
 
         model = AutoModelForSeq2SeqLM.from_pretrained("microsoft/GODEL-v1_1-large-seq2seq")
         tokenizer = AutoTokenizer.from_pretrained("microsoft/GODEL-v1_1-large-seq2seq")
-        super().__init__(model, tokenizer, device, beam_width, max_len)
+        super().__init__(model, tokenizer, device, generation_config)
 
         self.instruction = "Instruction: given a dialog context, you need to response empathically."
 
-    def generate(self, batch: Dict[str, Tuple]) -> Tuple[List[List[int]], float]:
+    def generate(self, batch: Dict[str, Tuple]) -> Tuple[Union[List[List[int]], float, int]]:
         _input = [f"{self.instruction} [CONTEXT] {' EOS '.join(dialogue)}"
                   for dialogue in batch["contexts"]]
         _input = self.tokenizer(_input, return_tensors="pt", padding=True).to(self.device)
@@ -59,19 +59,25 @@ class GODEL(PretrainedGenerationBase):
             decoder_input_ids=decoder_input.input_ids,
             decoder_attention_mask=decoder_input.attention_mask
         )
-        cross_entropy = F.cross_entropy(
+        sum_cross_entropy = F.cross_entropy(
             output.logits[:, :-1, :].permute(0, 2, 1),
             decoder_input.input_ids[:, 1:],
+            reduction="sum",
             ignore_index=self.tokenizer.pad_token_id
         )
+        num_tokens = torch.sum(decoder_input.input_ids[:, 1:] != self.tokenizer.pad_token_id)
 
         predictions = self.model.generate(
             _input.input_ids,
             pad_token_id=self.tokenizer.pad_token_id,
-            max_length=self.max_len,
-            num_beams=self.beam_width
+            max_new_tokens=self.generation_config.max_new_tokens,
+            num_beams=self.generation_config.beam_width,
+            do_sample=self.generation_config.sample,
+            temperature=self.generation_config.temperature,
+            top_p=self.generation_config.top_p,
+            top_k=self.generation_config.top_k
         )
 
-        return predictions.tolist(), cross_entropy
+        return predictions.tolist(), sum_cross_entropy, num_tokens
 
 # ----------------------------------------------------------------------------

test_pretrained.py
@@ -12,6 +12,7 @@ import torch
 
 # User-defined Modules
 from metric_utils import compute_test_metrics
+from data_classes import GenerationConfig
 
 # ------------------------- IMPLEMENTATION -----------------------------------
 
@@ -23,9 +24,13 @@ def main():
     parser.add_argument("--dialogue_model", type=str, default=None, required=True)
     parser.add_argument("--output_dir", type=str, default=None, required=True)
     parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--metric_n_grams", type=int, default=4)
     parser.add_argument("--beam_width", type=int, default=1)
-    parser.add_argument("--max_new_tokens", type=int, default=200)
-    parser.add_argument("--bleu_n_grams", type=int, default=4)
+    parser.add_argument("--sample", action="store_true")
+    parser.add_argument("--temperature", type=float, default=1.0)
+    parser.add_argument("--top_p", type=float, default=1.0)
+    parser.add_argument("--top_k", type=int, default=50)
+    parser.add_argument("--max_new_tokens", type=int, default=100)
     cli_args, _ = parser.parse_known_args()
 
     dataset_dir = os.path.abspath(cli_args.dataset_dir)
@@ -38,8 +43,14 @@ def main():
     gen_cls = getattr(__import__("pretrained_gen"), cli_args.dialogue_model)
     model_generation = gen_cls(
         device,
-        cli_args.beam_width,
-        cli_args.max_new_tokens
+        generation_config=GenerationConfig(
+            max_new_tokens=cli_args.max_new_tokens,
+            beam_width=cli_args.beam_width,
+            sample=cli_args.sample,
+            temperature=cli_args.temperature,
+            top_p=cli_args.top_p,
+            top_k=cli_args.top_k
+        )
     )
     model = model_generation.model
     tokenizer = model_generation.tokenizer
@@ -76,11 +87,13 @@ def main():
     )
 
     print("Generating predictions from pretrained model...")
-    predictions, enc_predictions, cross_entropy = [], [], []
+    predictions, enc_predictions = [], []
+    sum_cross_entropy, num_tokens = 0, 0
     with torch.no_grad():
         for batch in tqdm(test_dataloader):
-            preds, nll = model_generation.generate(batch)
-            cross_entropy.append(nll)
+            preds, sum_ce, num = model_generation.generate(batch)
+            sum_cross_entropy += sum_ce
+            num_tokens += num
             enc_predictions.extend(preds)
             predictions.extend([tokenizer.decode(enc, skip_special_tokens=True) for enc in preds])
     print("Done.")
@@ -114,9 +127,9 @@ def main():
         enc_targets,
         enc_predictions,
         model.get_input_embeddings(),
-        cli_args.bleu_n_grams,
+        cli_args.metric_n_grams,
     )
-    test_metrics["ppl"] = math.exp(sum(cross_entropy) / len(cross_entropy))
+    test_metrics["ppl"] = math.exp(sum_cross_entropy / num_tokens)
     print("Test metrics computed.")
 
     fname = f"{dir}/test_metrics.json"
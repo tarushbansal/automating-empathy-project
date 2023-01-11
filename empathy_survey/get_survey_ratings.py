2f7dcee695105b186591067918513b5c8f9a6c80
T. Bansal <tb662@login-q-4.data.cluster>
2023-01-11 07:49:41 +0000

    Fixed decimal save bug in get survey ratings

--- empathy_survey/get_survey_ratings.py
+++ empathy_survey/get_survey_ratings.py
@@ -5,6 +5,7 @@ import os
 import json
 import logging
 from stat import S_IREAD
+from decimal import Decimal
 from boto3.dynamodb.conditions import Attr
 
 # User-Defined Modules
@@ -13,11 +14,27 @@ from dynamodb_utils import retrieve_dynamodb_table
 # ------------------------- IMPLEMENTATION -----------------------------------
 
 logger = logging.getLogger(__name__)
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Preset backend sample status codes
+backend_config = f"{working_dir}/backend_config.json"
+if not os.path.isfile(backend_config):
+    raise FileNotFoundError("File 'backend_config.json' does not exist in working directory")
+with open(backend_config) as f:
+    try:
+        COMPLETE = json.load(f)["status_codes"]["COMPLETE"]
+    except KeyError:
+        raise KeyError("Status code 'PENDING' not specified in 'backend_config.json'")
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return int(obj)
+        return json.JSONEncoder.default(self, obj)
+
 
 if __name__ == "__main__":
-    # Present backend variable
-    COMPLETE = 2
-
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
     samples_table = retrieve_dynamodb_table("samples")
@@ -31,7 +48,7 @@ if __name__ == "__main__":
         dirname = os.path.dirname(os.path.abspath(__file__))
         ratings_fpath = os.path.join(dirname, "ratings.json")
         with open(ratings_fpath, "w") as f:
-            json.dump(rated_samples, f)
+            json.dump(rated_samples, f, cls=DecimalEncoder)
             logger.info(f"Saved ratings at {ratings_fpath}")
         os.chmod(ratings_fpath, S_IREAD)
 

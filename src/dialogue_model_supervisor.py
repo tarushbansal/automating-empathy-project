Fixed logging issues. Added entropy loss in rlhf setting kl penalty to 0 temporarily

src/dialogue_model_supervisor.py
@@ -76,7 +76,7 @@ class DialogueModelSupervisor(pl.LightningModule):
     ) -> float:
         output = self.forward(batch)
         labels = batch.targets[:, 1:] if self.model.has_encoder else batch.dialogues[:, 1:]
-        loss = F.cross_entropy(
+        lm_loss = F.cross_entropy(
             output.logits[:, :-1, :].permute(0, 2, 1),
             labels,
             ignore_index=self.tokenizer.PAD_IDX
@@ -89,39 +89,42 @@ class DialogueModelSupervisor(pl.LightningModule):
             )
             self.log(
                 f"{stage}_emo_loss", 
-                loss, 
-                prog_bar=True, 
-                on_step=True, 
+                emo_loss,
                 on_epoch=True, 
                 batch_size=self.batch_size, 
                 sync_dist=True
             )
-            self.logger.experiment.add_scalars('emo_loss', {stage: emo_loss}, self.global_step)
         if hasattr(self.model, "emo_attn_loss"):
             emo_attn_loss = self.model.emo_attn_loss
             self.log(
                 f"{stage}_emo_attn_loss", 
-                loss, 
-                prog_bar=True, 
-                on_step=True, 
+                emo_attn_loss,
                 on_epoch=True, 
                 batch_size=self.batch_size, 
                 sync_dist=True
             )
-            self.logger.experiment.add_scalars(
-                'emo_attn_loss', {stage: emo_attn_loss}, self.global_step)
+        
+        loss = lm_loss + 1 * emo_loss + 0.1 * emo_attn_loss
+
         self.log(
             f"{stage}_loss", 
             loss, 
             prog_bar=True, 
-            on_step=True, 
-            on_epoch=True, 
+            on_epoch=True,
             batch_size=self.batch_size, 
             sync_dist=True
         )
-        self.logger.experiment.add_scalars('loss', {stage: loss}, self.global_step)
-        
-        return loss + 1 * emo_loss + 0.1 * emo_attn_loss
+
+        if loss != lm_loss:
+            self.log(
+                f"{stage}_lm_loss", 
+                loss,
+                on_epoch=True,
+                batch_size=self.batch_size, 
+                sync_dist=True
+            )
+
+        return loss
 
     def training_step(
         self, 

src/rlhf_supervisor.py
@@ -126,6 +126,13 @@ class RLHFSupervisor(pl.LightningModule):
                     mask=reward_model_inputs.attention_mask.to(device)
                 )
             )
+            self.log(
+                f"{stage}_reward", 
+                torch.mean(scores), 
+                on_epoch=True, 
+                batch_size=self.batch_size, 
+                sync_dist=True
+            )
         
         # OPTIMIZATION PHASE
         if self.tuned_model.model.has_encoder:
@@ -155,6 +162,9 @@ class RLHFSupervisor(pl.LightningModule):
         pad_mask = (labels != self.tuned_model.tokenizer.PAD_IDX)
         gen_len = torch.sum(pad_mask, dim=-1)
         new_log_probs = self.log_probs(out.logits[:, :-1, :], labels, pad_mask)
+        entropy = -torch.sum(
+            torch.softmax(out.logits[:, :-1, :], dim=-1) * 
+                torch.log_softmax(out.logits[:, :-1, :], dim=-1), dim=-1)
         values = self.value_head(last_hidden_states).squeeze(-1)
         with torch.no_grad():
             logits = self.ref_model.forward(batch).logits
@@ -166,33 +176,25 @@ class RLHFSupervisor(pl.LightningModule):
         rewards = self.compute_rewards(scores, new_log_probs, ref_log_probs, gen_len)
         advantages = self.estimate_advantages(rewards, values, gen_len)
         returns = advantages + values
-        
+
         # Apply PPO Algorithm (With policy and value function losses)
-        vf_loss = (values - returns) ** 2
-        clipped_values = torch.max(torch.min(
-                values, 
-                values + self.ppo_config.value_clip_range), 
-            values - self.ppo_config.value_clip_range
-        )
-        vf_loss_clipped = (clipped_values - returns) ** 2
-        vf_loss = torch.sum(
-            torch.max(vf_loss, vf_loss_clipped) * pad_mask
-            ) / torch.sum(pad_mask)
+        num_tokens = torch.sum(pad_mask)
+        vf_loss = torch.sum(((values - returns) ** 2) * pad_mask) / num_tokens
+        entropy_loss = -torch.sum(entropy * pad_mask) / num_tokens
         ppo_loss = torch.sum(
-            torch.max(-ratios * advantages, -clipped_ratios * advantages) * pad_mask
-            ) / torch.sum(pad_mask)
-        
-        loss = ppo_loss + self.ppo_config.vf_coeff * vf_loss
-        self.log(
-            f"{stage}_loss", 
-            loss, 
-            prog_bar=True, 
-            on_step=True, 
-            on_epoch=True, 
-            batch_size=self.batch_size, 
-            sync_dist=True
+            torch.max(-ratios * advantages, 
+                      -clipped_ratios * advantages) * pad_mask
+                ) / num_tokens
+        loss = (
+            ppo_loss + 
+            self.ppo_config.vf_coeff * vf_loss + 
+            self.ppo_config.entropy_coeff * entropy_loss
         )
-        self.logger.experiment.add_scalars('loss', {stage: loss}, self.global_step)
+        
+        self.log(f"{stage}_ppo_loss", ppo_loss, on_epoch=True, batch_size=self.batch_size, sync_dist=True)
+        self.log(f"{stage}_vf_loss", vf_loss, on_epoch=True, batch_size=self.batch_size, sync_dist=True)
+        self.log(f"{stage}_entropy_loss", entropy_loss, on_epoch=True, batch_size=self.batch_size, sync_dist=True)
+        self.log(f"{stage}_loss", loss, prog_bar=True, on_epoch=True, batch_size=self.batch_size, sync_dist=True)
         
         return loss
 